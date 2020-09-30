fe44b91656db724d4423b6b130977197c3887d56
Lionuncle <goninside@gmail.com>
2020-09-30 12:19:44 +0500

    Bottom navigation added

---
+++ app/src/main/java/com/lionuncle/teamscope/FormResultFragment.kt
@@ -0,0 +1,17 @@
+package com.lionuncle.teamscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FormResultFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_form_result, container, false)
+    }
+
+}
--- app/src/main/java/com/lionuncle/teamscope/MainActivity.kt
+++ app/src/main/java/com/lionuncle/teamscope/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lionuncle.teamscope
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavView.setupWithNavController(navController)
     }
+
 }
